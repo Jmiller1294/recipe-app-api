5e36ffb07357e8540155e0566b45238abd00da19
Jmiller1294 <Jayman1294@gmail.com>
2022-01-06 01:40:54 -0500

    created user model tests

--- app/core/tests/tests_models.py
+++ app/core/tests/tests_models.py
@@ -5,3 +5,12 @@ class ModelTests(TestCase):
 
   def test_create_user_with_email_successful(self):
     """Test creating a new user with an email is successful"""
+    email = 'test@gmail.com'
+    password = 'pass123'
+    user = get_user_model().objects.create_user(
+      email=email,
+      password=password
+    )  
+
+    self.assertEqual(user.email, email)
+    self.assertEqual(user.check_password(password))
